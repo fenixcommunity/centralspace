4e2ca95bbb035f09ad65509d932211d2d1fe2615
Maksymilian Kaminski <m7.kaminski@gmail.com>
2019-01-28 20:41:30 +0100

    #centralspace. Init centralspace for Fenix Community App

---
+++ src/main/java/com/fenixcommunity/centralspace/CentralspaceApplication.java
@@ -0,0 +1,14 @@
+package com.fenixcommunity.centralspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralspaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CentralspaceApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/com/fenixcommunity/centralspace/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.fenixcommunity.centralspace;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CentralspaceApplication.class);
+    }
+
+}
+

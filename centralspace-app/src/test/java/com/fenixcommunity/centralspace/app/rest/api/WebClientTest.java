415b0386a91d76ff94f73a15d1bce0ddf539ecda
Maksymilian Kaminski <m7.kaminski@gmail.com>
2020-01-27 21:31:23 +0100

    web client test implementation 2

--- centralspace-app/src/test/java/com/fenixcommunity/centralspace/app/rest/api/WebClientTest.java
+++ centralspace-app/src/test/java/com/fenixcommunity/centralspace/app/rest/api/WebClientTest.java
@@ -3,7 +3,11 @@ package com.fenixcommunity.centralspace.app.rest.api;
 import com.fenixcommunity.centralspace.app.configuration.CentralspaceApplicationConfig;
 import com.fenixcommunity.centralspace.app.configuration.restcaller.RestCallerStrategy;
 import com.fenixcommunity.centralspace.app.configuration.restcaller.webclient.WebClientConfig;
+import com.fenixcommunity.centralspace.app.rest.dto.AccountDto;
+import com.fenixcommunity.centralspace.app.rest.dto.responseinfo.BasicResponse;
+import com.fenixcommunity.centralspace.app.service.AccountService;
 import com.fenixcommunity.centralspace.domain.model.mounted.account.Account;
+import com.fenixcommunity.centralspace.utilities.common.Level;
 import org.assertj.core.api.Assertions;
 import org.junit.FixMethodOrder;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,20 +16,31 @@ import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import reactor.core.publisher.Mono;
 
+import javax.validation.Valid;
 import java.time.ZonedDateTime;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 import static com.fenixcommunity.centralspace.app.configuration.security.autosecurity.SecurityRole.ADMIN;
 import static com.fenixcommunity.centralspace.app.configuration.security.autosecurity.SecurityRole.BASIC;
-import static com.fenixcommunity.centralspace.utilities.common.Var.PASSWORD;
+import static com.fenixcommunity.centralspace.app.rest.mapper.AccountMapper.mapToDto;
+import static com.fenixcommunity.centralspace.utilities.common.Var.*;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.web.reactive.function.client.ExchangeFilterFunctions.basicAuthentication;
 
@@ -42,16 +57,22 @@ class WebClientTest {
     private WebTestClient adminClient;
     private WebTestClient basicClient;
 
+    private AccountDto accountDto;
+
     @Autowired
     private RestCallerStrategy restCallerStrategy;
 
     @LocalServerPort
     private String port;
 
+    @MockBean
+    private AccountService accountService;
+
     @BeforeEach
     public void init() {
         this.basicClient = setOptions(BASIC.name());
         this.adminClient = setOptions(ADMIN.name());
+        initAccount();
     }
 
     private WebTestClient setOptions(String user) {
@@ -67,6 +88,18 @@ class WebClientTest {
                 })
                 .cookies(cookie -> cookie.add("cookieTest", "cookieValue"));
         return webTestClient;
+    }
+
+    private void initAccount() {
+//            .apply(SecurityMockMvcConfigurers.springSecurity())
+        Account account = Account.builder()
+                .id(ID)
+                .login(LOGIN)
+                .mail(MAIL)
+                .passwords(Collections.singletonList(null))
+                .build();
+        accountDto = mapToDto(account, Level.HIGH);
+        when(accountService.findById(ID)).thenReturn(Optional.of(account));
     }
 
     @Test
@@ -87,24 +120,31 @@ class WebClientTest {
 
     @Test
     void testAccountGetAllAsAdmin() {
-        adminClient.get()
-                .uri(BASE_ACCOUNT_URL + "all")
-
-                .body(Mono.just(newRepoDetails), RepoRequest.class)
-
+        EntityExchangeResult<List<Account>> result = adminClient.get()
+                .uri(BASE_ACCOUNT_URL + "{id}", 1L)
                 .exchange()
                 .expectStatus().is2xxSuccessful()
                 .expectHeader().contentType(MediaType.APPLICATION_JSON)
-                .expectBodyList(Account.class);
+                .expectBodyList(Account.class)
+                .consumeWith(response ->
+                        Assertions.assertThat(response.getResponseBody()).isNotNull())
+                .returnResult();
+
+        List<Account> responseBody = result.getResponseBody();
+
+                .body(Mono.just(accountDto), AccountDto.class)
+
+        @PostMapping("/create")
+        @ResponseStatus(HttpStatus.CREATED)
+        public ResponseEntity<BasicResponse> create ( @Valid @RequestBody final AccountDto accountDto){
+            final Long generatedId = accountService.save(createdAccount).getId();
 
                 .jsonPath("$.name").isEqualTo("updated-webclient-repository");
-                .returnResult(ResponseEntity.class);
-                .consumeWith(response ->
-                Assertions.assertThat(response.getResponseBody()).isNotNull());
+
     }
 
     @Test
-    void testGetAllAsBasic() {
+        void testAccountGetAllAsBasic () {
         basicClient
                 .get()
                 .uri(BASE_ACCOUNT_URL + "all")
